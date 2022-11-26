7a56d1964f8b77099605afaa138566ffdbdfd313
RZhang <richardzhiyuanzhang@gmail.com>
2022-11-25 21:10:46 -0500

    add a base.html template. update views to use template enheritance using base as the base layout.

--- meetups/urls.py
+++ meetups/urls.py
@@ -6,6 +6,6 @@ from . import views
 # registers all urls for the app and which view functions should be executed
 # when requests reach those urls
 urlpatterns = [
-    path('meetups/', views.index),  # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details) # our-domain.com/meetups/<dynamic-path-segment>
+    path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups. name uniquely identifies this path
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-details') # our-domain.com/meetups/<dynamic-path-segment>
 ]
