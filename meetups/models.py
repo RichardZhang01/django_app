b14c73aac644c494aec524787e13b9bb39915bf5
RZhang <richardzhiyuanzhang@gmail.com>
2022-11-26 21:33:11 -0500

    create Meetup model

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200) # CharField is for shorter text
+    description = models.TextField() # TextField is for longer text
+    slug = models.SlugField(unique=True) # Specific for slugs. ALso generates an ID based on the slug which makes querying for the slug easier
