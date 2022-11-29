3c2431e462bb023f1a2c49a2840f8c564c3e767b
RZhang <richardzhiyuanzhang@gmail.com>
2022-11-28 21:17:26 -0500

    implement image uploads/serving

--- meetups/models.py
+++ meetups/models.py
@@ -6,3 +6,4 @@ class Meetup(models.Model):
     title = models.CharField(max_length=200) # CharField is for shorter text
     slug = models.SlugField(unique=True) # Specific for slugs. ALso generates an ID based on the slug which makes querying for the slug easier
     description = models.TextField() # TextField is for longer text
+    image = models.ImageField(upload_to='images')
