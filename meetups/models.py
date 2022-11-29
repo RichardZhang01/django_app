250072dde376c8f7a9c8077c23ad4a2fdd97981e
RZhang <richardzhiyuanzhang@gmail.com>
2022-11-29 15:55:53 -0500

    create Location model and associations between Location and Meetup. Add columns and filters to admin panel

--- meetups/models.py
+++ meetups/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200) # CharField is for shorter text
     slug = models.SlugField(unique=True) # Specific for slugs. ALso generates an ID based on the slug which makes querying for the slug easier
     description = models.TextField() # TextField is for longer text
     image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} - {self.slug}'
